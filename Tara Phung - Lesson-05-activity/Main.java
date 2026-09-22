fba14e7b76a2ff902068f44410bf149cf2ececca
Tara Phung <tarap15@nycstudents.net>
2026-09-22 17:50:02 +0000

    s

---
+++ Tara Phung - Lesson-05-activity/Main.java
@@ -0,0 +1,127 @@
+class Main {
+	public static void main(String[] args) {
+    	(new Main()).init();
+	}
+
+  void init(){
+    
+
+/*  
+    Challenge 1:
+    Create two integer variables and Assign values to them. 
+    Calculate the sum of the two numbers and store the 
+    calculated sum and then display it.
+*/
+
+int one = -5;
+int two = 10;
+
+double three = one + two;
+
+System.out.println(three);
+   
+
+/*  
+    Challenge 2:
+    Create three variables to assign three grades and Assign values to each grade. 
+    Calculate the sum of the three grades and store the 
+    calculated sum and then display it.
+    
+*/
+
+int grade1 = 85;
+int grade2 = 90;
+int grade3 = 72;
+
+double sum = grade1 + grade2 + grade3;
+
+System.out.println(sum);
+
+
+/*  
+    Challenge 3:
+    Calculate the average from the three grades from challenge 2,
+    store the value and then display it.
+    Declare and assign values to any new variables
+    NOTE: Does it look correct, check with a calculator?
+*/
+
+double avg = (grade1 + grade2 + grade3) / 3.0;
+
+System.out.println(avg);
+
+/*  
+    Challenge 4:
+    Write the following equation in EQ1.PNG file in Java; store the result and the display it:
+    Declare and assign values to any new variables
+
+*/
+
+double A = 3.0;
+double x = 22.0;
+
+double y = A / (x+1);
+
+System.out.println(y);
+
+
+/*  
+    Challenge 5:
+    Using the variables same variables from challenge4 above, write the following equation in EQ2.PNG file in Java, store the result and the display it:
+
+    Declare and assign values to any new variables
+
+*/
+ 
+y = ((2*x)*(x+1)*(-x/2)) / A;
+System.out.println(y);
+
+
+/*  
+    Challenge 6:
+    Create the variables and write the equation in
+    file  EQ3.PNG
+
+    Declare and assign values to any new variables
+*/
+ 
+ int b = 5;
+ int h = 15;
+
+ double area = 1.0/2 * b * h;
+ System.out.println(area);
+
+
+
+/*  
+    **** Bonus Challenge ****:
+    Create a variable that stores the total number of eggs 
+    and assign it 100. We want to fill as many baskets with 
+    eggs as we can. Each basket can hold only 12 eggs.
+
+    1) Write the java code that will calcute how many baskets
+    of 12 eggs can we fill fully.
+
+    HINT: What do we get when we divide an integer by 
+    an integer in Java
+
+    2) Write the java code that will calculate how many eggs
+    are left over after we filled as many baskets of 12 eggs.
+*/
+
+int eggs = 100;
+int basket = 12;
+
+
+double total = 100/12;
+System.out.println(total);
+
+double leftover = 100%12;
+System.out.println(leftover);
+
+
+    // **************************************************
+    // **** Don't write any code below here.  ***********
+    // **************************************************
+  }
+}
